25490f23c194854bf7984b0445a9feb7869c4ea6
suraj <surazgyawali@gmail.com>
2019-06-03 02:24:03 +0545

    simple template with services dropdown and placeholder to map implemented

--- office_locator.py
+++ office_locator.py
@@ -6,6 +6,17 @@ import petl
 DATA_FILE = "./office_service_locations.csv"
 SERVICES_FILE = "./Office_Services.csv"
 
+@bottle.route('/static/<filepath:path>')
+def server_static(filepath):
+    return bottle.static_file(filepath, root='views/static/')
+
+@bottle.route("/")
+def home():
+    data_table =  petl.fromcsv(SERVICES_FILE)
+    data = [("Business & Trade",1),("Life Events",2),("Legal Information & Services",3),("Employment & Jobs",4),("Health & Wellbeing",5)]
+    print(data)
+    return bottle.template('office_map',data=data)
+
 @bottle.route("/getoffices")
 def get_offices():
     serviceID = bottle.request.query.serviceid
